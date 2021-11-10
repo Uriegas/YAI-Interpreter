f4303f48867aca95a16d277867315ddfc6374516
1930526@upv.edu.mx <1930526@upv.edu.mx>
2021-11-10 11:01:57 -0600

    Terminals

---
+++ java/com/uriegas/yai/TokenType.java
@@ -0,0 +1,22 @@
+package com.uriegas.yai;
+
+enum TokenType {
+    // Single-character tokens.
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // One or two character tokens.
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    // Literals.
+    IDENTIFIER, STRING, NUMBER,
+
+    // Keywords.
+    AND, ELSE, FALSE, FUN, FOR, IF, NULL, OR,
+    PRINT, RETURN, TRUE, VAR, WHILE,
+
+    EOF
+}
