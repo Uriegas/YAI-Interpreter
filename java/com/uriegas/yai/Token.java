27cda5cdc3ee64c5e210e8add44918aacefd889a
1930526@upv.edu.mx <1930526@upv.edu.mx>
2021-11-10 11:58:37 -0600

    Verbose token

--- java/com/uriegas/yai/Token.java
+++ java/com/uriegas/yai/Token.java
@@ -1,19 +1,29 @@
 package com.uriegas.yai;
 
+/**
+ * Representation of a Token
+ */
 class Token {
-  final TokenType type;
-  final String lexeme;
-  final Object literal;
-  final int line; 
+    final TokenType type;
+    final String lexeme;
+    final Object literal;
+    final int line; 
 
-  Token(TokenType type, String lexeme, Object literal, int line) {
-    this.type = type;
-    this.lexeme = lexeme;
-    this.literal = literal;
-    this.line = line;
-  }
+    /**
+     * Constructor
+     * @param type TokenType
+     * @param lexeme String
+     * @param literal Object
+     * @param line int
+     */
+    Token(TokenType type, String lexeme, Object literal, int line) {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+    }
 
-  public String toString() {
-    return type + " " + lexeme + " " + literal;
-  }
+    public String toString() {
+        return type + " " + lexeme + " " + literal;
+    }
 }
